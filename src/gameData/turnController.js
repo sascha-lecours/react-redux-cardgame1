8f9b9947801c81e4648c28b27faf475787584cfe
slecours <sascha.lecours@gmail.com>
2019-02-18 15:12:21 -0500

    added blueprint for turn controller component

---
+++ src/gameData/turnController.js
@@ -0,0 +1,28 @@
+// A component that manages the game state
+// This may be best handled using a subscribe listener that checks for certain key changes in...
+// ... in the store and implements turn events accordingly
+
+
+// -> Dormant state when awaiting combat
+
+// -> Initialize combat.
+// --> Fetch player stats and deck(s), possibly including modified cards, buffs/debuffs, etc.
+// --> Fetch enemy stats
+
+// -> Begin turn loop.
+// 1. Deal cards
+// --> Deal player hand(s), including placing 'innate' cards in first time through.
+// 2. Assign enemy moves
+// 3. Player start-of-turn effects, checking for combat-over
+// 4. Player plays card
+// 5. Resolve all card effects, including visuals/sound and checking for combat-over
+// 6. Player turn ends (muliple cards?) - player end-of-turn effects
+// 7. Enemy start-of-turn effects, checking for combat-over
+// 8. Enemy actions resolve, up through array, checking for combat-over
+// 9. Enemy end-of-turn effects, checking for combat-over
+// 10. Increment turn counter, start back at step 1.
+
+// -> Resolve post-combat
+// 1. End-of-combat effects such as healing
+// 2. Gain rewards (gold, relics, potions, cards, etc.)
+// 3. Hand off to "campaign" component that moves on  higher scope.
